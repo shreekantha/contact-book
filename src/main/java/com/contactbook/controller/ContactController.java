02f08eb1c2a63ef9ae2736aea177319f69a800cd
Shreekantha <shreekanth.karikalli@gmail.com>
2018-11-03 13:03:11 +0530

    test method added

--- src/main/java/com/contactbook/controller/ContactController.java
+++ src/main/java/com/contactbook/controller/ContactController.java
@@ -3,6 +3,7 @@ package com.contactbook.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,5 +29,9 @@ public class ContactController {
 		
 		
 		return contactService.addContact(contact);
+	}
+	@GetMapping
+	public String test() {
+		return "Welcome to Contact Book";
 	}
 }
